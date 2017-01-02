bf6b8cd3c0bf9a446252d3af9255426a63c022fe
makena <roselynemakena@gmail.com>
2017-01-02 15:27:29 +0300

    Added login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'about', to: 'welcome#about'
 
+
+  devise_scope :user do
+  get 'login', to: 'devise/sessions#new'
+  get 'logout', to: 'devise/sessions#destroy'
+  end
  
 end
